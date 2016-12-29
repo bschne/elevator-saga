cdd3dfdba643ebd0db5f710d3f819cf41d7300fa
Benjamin Schneider <mail@benjaminschneider.ch>
2016-12-29 11:02:26 +0100

    use destination queue

--- elevatorsaga.js
+++ elevatorsaga.js
@@ -2,18 +2,22 @@
     init: function(elevators, floors) {
         var elevator = elevators[0]; // Let's use the first elevator
 
-        // Handle floor button presses
-        elevator.on("floor_button_pressed", function(floorNum) {
-        	elevator.goToFloor(floorNum);
-        });
+        // Set up elevator behaviour
+        elevators.forEach(function(elevator) {
+        	// Handle floor button presses
+	        elevator.on("floor_button_pressed", function(floorNum) {
+	        	elevator.destinationQueue.push(floorNum);
+	        	elevator.checkDestinationQueue;
+	        });
 
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function() {
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
-            elevator.goToFloor(2);
-            elevator.goToFloor(3);
-            elevator.goToFloor(4);
+	        // Whenever the elevator is idle (has no more queued destinations) ...
+	        elevator.on("idle", function() {
+	            elevator.goToFloor(0);
+	            elevator.goToFloor(1);
+	            elevator.goToFloor(2);
+	            elevator.goToFloor(3);
+	            elevator.goToFloor(4);
+	        });
         });
     },
     update: function(dt, elevators, floors) {
