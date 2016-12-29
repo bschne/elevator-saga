f63011eb2588ae123cc4bd13c7a146bb43e5850d
Benjamin Schneider <mail@benjaminschneider.ch>
2016-12-29 10:57:25 +0100

    Handle floor button press

--- elevatorsaga.js
+++ elevatorsaga.js
@@ -2,11 +2,18 @@
     init: function(elevators, floors) {
         var elevator = elevators[0]; // Let's use the first elevator
 
+        // Handle floor button presses
+        elevator.on("floor_button_pressed", function(floorNum) {
+        	elevator.goToFloor(floorNum);
+        });
+
         // Whenever the elevator is idle (has no more queued destinations) ...
         elevator.on("idle", function() {
-            // let's go to all the floors (or did we forget one?)
             elevator.goToFloor(0);
             elevator.goToFloor(1);
+            elevator.goToFloor(2);
+            elevator.goToFloor(3);
+            elevator.goToFloor(4);
         });
     },
     update: function(dt, elevators, floors) {
