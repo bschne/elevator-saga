50039249378223a7b6975bc17ba7294959d9171d
Benjamin Schneider <mail@benjaminschneider.ch>
2016-12-29 11:24:22 +0100

    Put pressed floors in a queue

--- elevatorsaga.js
+++ elevatorsaga.js
@@ -1,9 +1,12 @@
 {
     init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
+        var floorQueue = [];
 
         // Set up elevator behaviour
         elevators.forEach(function(elevator) {
+        	// Initialize destination queue so idle elevators start on floor 0
+        	elevator.destinationQueue = [0];
+
         	// Handle floor button presses
 	        elevator.on("floor_button_pressed", function(floorNum) {
 	        	elevator.destinationQueue.push(floorNum);
@@ -12,12 +15,22 @@
 
 	        // Whenever the elevator is idle (has no more queued destinations) ...
 	        elevator.on("idle", function() {
-	            elevator.goToFloor(0);
-	            elevator.goToFloor(1);
-	            elevator.goToFloor(2);
-	            elevator.goToFloor(3);
-	            elevator.goToFloor(4);
+	            elevator.destinationQueue.push(floorQueue.shift());
+	            elevator.checkDestinationQueue();
 	        });
+        });
+
+        // Set up floor behaviour
+        floors.forEach(function(floor) {
+        	floor.on("up_button_pressed", function() { 
+        		floorQueue.push(floor.floorNum());
+        		console.log(floorQueue);
+        	});
+
+        	floor.on("down_button_pressed", function() { 
+        		floorQueue.push(floor.floorNum());
+        		console.log(floorQueue);
+        	});
         });
     },
     update: function(dt, elevators, floors) {
